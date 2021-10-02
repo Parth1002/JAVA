da569b9ff8c688f165e67d02d6ee06443f874397
Parth1002 <72753877+Parth1002@users.noreply.github.com>
2021-10-02 22:23:05 +0530

    java

---
+++ Pa_56_Collection_ArrayList.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Pa_56_Collection_ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> pa1 = new ArrayList<>();
+        ArrayList<Integer> pa2 = new ArrayList<>();
+
+        // Add Arraylist
+        pa1.add(2);
+        pa1.add(8);
+        pa1.add(7);
+        pa1.add(2);
+        pa1.add(4);
+
+        pa2.add(52);
+        pa2.add(58);
+        pa2.add(57);
+        pa2.add(58);
+        pa2.add(54);
+
+        // Remove Arraylist
+        // pa1.remove(3);
+
+        // Value true & false Arraylist
+        // System.out.println(pa1.contains(7));
+
+        // Clear Arraylist
+        // pa1.clear();
+
+
+        // AddAll Arraylist
+        // pa1.addAll(5,pa2);
+        
+        //Indexof Arraylist
+        // System.out.println(pa2.indexOf(54));
+
+        //lastOf Arraylist
+        // System.out.println(pa2.lastIndexOf(58));
+
+        System.out.println(pa1+" "+pa2);
+
+    }
+}
